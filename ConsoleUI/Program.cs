d53520037ebabd3de435965e2344564bbdf6dd53
Arda <arda.acar16@hotmail.com>
2021-01-31 19:55:12 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ModelYear + " Model - Günlük " + car.DailyPrice + " TL - " + car.Description);
+            }
+        }
+    }
+}
